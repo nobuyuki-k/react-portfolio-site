81e6f39aa2d91690487168db0408257ac568e306
nobuyuki-k <nobuyuki.komai@gmail.com>
2022-02-27 12:56:13 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // Headerコンポーネントをimportする
 import { Header } from './components/Header';
 import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 function App() {
   return (
     <>
